5b0e2e4dec4c7be6e76bd4ed2500422c1beb17ac
snootnoot <54415463+Snootnoot@users.noreply.github.com>
2022-02-04 19:50:07 -0800

    DOX

--- rolle.py
+++ rolle.py
@@ -4,7 +4,7 @@ import time
 from pynput.keyboard import Key, Controller
 from selenium import webdriver
 keyboard = Controller()
-browser = webdriver.Chrome('C:/Users/samso/Documents/chromedriver')
+browser = webdriver.Chrome('C:/Users/snootnoot/Documents/chromedriver')
 browser.get('https://www.youtube.com/watch?v=oHg5SJYRHA0')
 keyboard.press(Key.space)
 keyboard.release(Key.space)
